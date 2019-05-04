2ce56d13289dd35d5b7b07b74de4acdf0a4867a6
Abagail Hudson <abagailanne@gmail.com>
2019-05-04 23:52:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,11 @@
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(array)
+  new_array = []
+  array.each do |name|
+    new_array<<"Hello, my name is #{name}."
+  end
+  return new_array
+end
