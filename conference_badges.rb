41e393d02dc9a7a4b99c4bb22d0591748a81c0eb
Abagail Hudson <abagailanne@gmail.com>
2019-05-05 00:12:42 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -22,7 +22,10 @@ def assign_rooms(array)
 end
 
 def printer(array)
-   batch_badge_creator(array).each do |name|
+  batch_badge_creator(array).each do |name|
      puts name
+  end
+  assign_rooms(array).each do |name|
+    puts name
   end
 end
